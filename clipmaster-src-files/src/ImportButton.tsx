35803f64f17609be00e4e498adb44c21c1a7e62e
meloniouss <andrei.jianu31@gmail.com>
2024-12-09 03:47:23 -0500

    started testing ffmpeg

--- clipmaster-src-files/src/ImportButton.tsx
+++ clipmaster-src-files/src/ImportButton.tsx
@@ -1,15 +1,49 @@
 import {Button} from '@mui/material'
 import './zustandStore'
 import { useStore } from './zustandStore';
+import { FFmpeg } from '@ffmpeg/ffmpeg';
+import { useRef } from 'react';
+import { fetchFile, toBlobURL } from '@ffmpeg/util';
 
 
 export default function ImportButton() {
   const { importFile, setProxyFile, mainFile, proxyFile } = useStore();
-
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const ffmpegRef = useRef(new FFmpeg());
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
+    console.log("CHANGE!")
     if (file) {
-      importFile(file); 
+      console.log('importing file!')
+      importFile(file);
+      const ffmpeg = ffmpegRef.current; //TODO: maybe set the ffmpeg ref in zustand so as to not reload it every time? if thats how it would work
+      console.log('loading ffmpeg')
+      const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.6/dist/esm'
+      await ffmpeg.load({
+        coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
+        wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
+      }); 
+      console.log('ffmpeg loaded!')
+      console.log('fetching file')
+
+      await ffmpeg.writeFile(file.name, await fetchFile(file));
+      console.log('transcoding video')
+      await ffmpeg.exec([
+        '-i', file.name,                      
+        '-vf', 'scale=1280:720',              // resize to 720p
+        '-preset', 'ultrafast',
+        '-tune', 'zerolatency',
+        'output.mp4',                   
+      ]);
+
+      console.log('video successfully transcoded!')
+
+      const data = await ffmpeg.readFile('output.mp4');
+      const outputFile = new File([data], "output.mp4", { type: "video/mp4" });
+      console.log('output file generated')
+
+      setProxyFile(outputFile);
+
+      console.log('Proxy file set!');
     }
   };
 return(
@@ -25,6 +59,6 @@ return(
         onChange={handleFileChange}
       />
       </Button>
-            {mainFile && <video src={mainFile.url} controls />}
+            {proxyFile && <video src={proxyFile.url} controls />}
     </>
 )};
