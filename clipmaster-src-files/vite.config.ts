b6eed36ddb14f1531602ce51fc5163ccf036f748
meloniouss <andrei.jianu31@gmail.com>
2025-01-12 16:05:26 -0500

    fixed vite config file

--- clipmaster-src-files/vite.config.ts
+++ clipmaster-src-files/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fetchFile, toBlobURL } from '@ffmpeg/util';
 
 // https://vite.dev/config/
 export default defineConfig({
